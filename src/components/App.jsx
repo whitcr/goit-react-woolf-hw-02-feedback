ce8d860a4f4afd66553a08f497169697a7da6a93
whitcr <whitcr0k@gmail.com>
2024-04-08 11:03:59 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -49,7 +49,7 @@ export class App extends Component {
               positivePercentage={positivePercentage}
             />
           ) : (
-            <Notification message="There is no feedback" />
+            <Notification message="There is no feedback " />
           )}
         </Section>
       </div>
